4cdfe121eebd35cc797490d649809e4dcc0d2238
Andy Xiao <andyxiao@seas.upenn.edu>
2018-12-25 03:39:24 -0500

    update readme

---
+++ src/components/Board.js
@@ -0,0 +1,116 @@
+import React from "react";
+import "./Board.css";
+
+function Cell(props) {
+  return (
+    <button
+      className="cell"
+      onClick={props.onClick}
+    >
+      {props.value ? "O" : null}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: this.reset(),
+    };
+    this.tick = this.tick.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  reset() {
+    const size = 15;
+    var newSquares = [];
+    for (var i = 0; i < size; i++) {
+      newSquares[i] = Array(size).fill(false);
+    }
+    return newSquares;
+  }
+
+  tick() {
+    const oldSquares = this.state.squares.slice();
+    var newSquares = oldSquares.map(arr => arr.slice());
+
+    for (var r = 0; r < oldSquares.length; r++) {
+      for (var c = 0; c < oldSquares[r].length; c++) {
+        const aliveNeighbors = this.findAliveNeighbors(oldSquares, r, c);
+
+        if (aliveNeighbors < 2 || aliveNeighbors > 3) {
+          newSquares[r][c] = false;
+        } else if (aliveNeighbors === 3) {
+          newSquares[r][c] = true;
+        }
+      }
+    }
+    this.setState({squares: newSquares});
+  }
+
+  findAliveNeighbors(squares, row, col) {
+    const rowLimit = squares.length - 1;
+    const colLimit = squares[0].length - 1;
+    var aliveNeighbors = 0;
+
+    for (var i = Math.max(0, row - 1); i <= Math.min(rowLimit, row + 1); i++) {
+      for (var j = Math.max(0, col - 1); j <= Math.min(colLimit, col + 1); j++) {
+        if ((i !== row || j !== col) && squares[i][j]) {
+          aliveNeighbors++;
+        }
+      }
+    }
+    return aliveNeighbors;
+  }
+
+  handleClick(row, col) {
+    //var newSquares = this.state.squares.map(arr => arr.slice());
+    var newSquares = this.state.squares;
+    newSquares[row][col] = !newSquares[row][col];
+    this.setState({squares: newSquares});
+  }
+
+  renderSquare(row, col) {
+    return (
+      <Cell
+        value={this.state.squares[row][col]}
+        onClick={() => this.handleClick(row, col)}
+      />
+    );
+  }
+
+  renderRow(row) {
+    return (
+      <div className="board-row">
+        {this.state.squares[row].map((_, col) => this.renderSquare(row, col))}
+      </div>
+    );
+  }
+
+  render() {
+    const title = "Conway's Game of Life";
+    const squares = this.state.squares;
+
+    return (
+      <div>
+        <div className="status">{title}</div>
+        <span>
+          <button className="settings-buttons" onClick={() => this.tick()}>
+            Next
+          </button>
+        </span>
+        <span>
+          <button className="settings-buttons" onClick={() => this.setState({squares: this.reset()})}>
+            Reset
+          </button>
+        </span>
+        <div>
+          {squares.map((_, row) => this.renderRow(row))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Board;
